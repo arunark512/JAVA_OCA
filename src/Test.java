a0a7ef97235511166a8c99e7daf588f6c4a7df8c
ARK <arun.ark512@gmail.com>
2020-06-10 00:25:19 +0530

    New Project Created

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+
+public class Test {
+	public static void main(String[] args) throws InterruptedException {
+	Thread t=new Thread();
+	Object o=new Object();
+	String s=new String();
+	System.out.println(t==o);
+	System.out.println(o==s);
+//	System.out.println(i==s);
+	}
+
+}
